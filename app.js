d74f8bff9b0b72e8ff5ff1e62eb16f249f2c3c09
ifaxec <xecfahim.ahmed@gmail.com>
2024-11-03 19:03:52 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,43 +1,49 @@
 let myKey = config.MY_KEY;
 
-fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&q=travel&key=' + myKey)
-    .then(response => {
-        // network failure, request prevented
-        if (response.status >= 200 && response.status < 300) {
-            return response.json();
-        }
-    })
-    .then(data => {
-        // console.log(data)
-        const videoData = data.items.map((item)=>{
-            return item.snippet;
+
+const searchForm = document.querySelector('#search-form');
+
+searchForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const searchValue = document.querySelector('#search-bar').value;
+    console.log("🚀 ~ searchValue:", searchValue);
+
+    
+
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchValue}&maxResults=9&key=` + myKey)
+        .then(response => {
+            // network failure, request prevented
+            if (response.status >= 200 && response.status < 300) {
+                return response.json();
+            }
+        })
+        .then(data => {
+            // console.log(data)
+            const videoData = data.items.map((item) => {
+                return item.snippet;
+            });
+
+            // console.log(videoData)
+
+            const videoContainer = document.getElementById('video-container');
+            videoContainer.innerHTML = '';
+
+            videoData.forEach(video => {
+                const videoDiv = document.createElement('div');
+                videoDiv.classList.add('video-style-div');
+                videoDiv.innerHTML =
+                    `
+           <img src=${video.thumbnails.high.url}>
+           <h4> ${video.title} </h4>
+           <p>${video.channelTitle}</p>
+           <p> ${new Date(video.publishTime).toLocaleDateString()}</p>
+           `
+                videoContainer.appendChild(videoDiv);
+            });
         });
 
-        console.log(videoData)
-        
-        const videoContainer = document.getElementById('video-container');               
-        const videoDiv = document.createElement('div');
-        videoData.forEach(video => {
-           videoDiv.innerHTML = `
-           <img src=${video.thumbnails.high.url}>
-           `
-        
-       });
 
-        videoContainer.appendChild(videoDiv);
-
-    });
+});
 
 
 
-
-// var xhttp = new XMLHttpRequest();
-// xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//         // Typical action to be performed when the document is ready:
-//         const resData = JSON.parse(xhttp.responseText);
-//         console.log("🚀 ~ resData:", resData);
-//     }
-// };
-// xhttp.open("GET", "https://www.googleapis.com/youtube/v3/search?part=snippet&q=travel&key=" + myKey, true);
-// xhttp.send();
